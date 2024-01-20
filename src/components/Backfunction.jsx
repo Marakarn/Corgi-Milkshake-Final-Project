db94cad9f9eae21ab74e7188cb2fc7e9fa2dfbd2
Marakarn! <suttikarn1997@gmail.com>
2024-01-20 21:23:50 +0700

    Revert "Merge branch 'Karnner' into test-merge"
    This reverts commit 6f6e5a29c80d203bcd004d144cb547d3be10852d, reversing
    changes made to 0caeaa1653b6aec5466361d476241fef2a7c0ae1.

--- src/components/Backfunction.jsx
+++ src/components/Backfunction.jsx
@@ -18,5 +18,4 @@ function Backfunction({ path }) {
     </button>
     );
 }
-
 export default Backfunction;
--- src/components/Menubar.jsx
+++
@@ -1,8 +0,0 @@
-
-const Menubar = () => {
-    return (
-        <h1>test</h1>
-    )
-}
-
-export default Menubar
