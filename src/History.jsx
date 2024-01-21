c35b30c872afecb4eefe0785c3afec75d560223e
Marakarn! <suttikarn1997@gmail.com>
2024-01-21 12:09:27 +0700

    edit-history-page-unfinish

--- src/History.jsx
+++ src/History.jsx
@@ -1,8 +1,12 @@
 // import React from 'react'
+// import { useState } from 'react';
 import Layout from "./Layout"
 import Backfunction from "./components/Backfunction"
 
 const History = () => {
+
+    // const [section,setSection] = useState()
+    
     return (
         <>
         <Layout>
@@ -10,12 +14,6 @@ const History = () => {
                 <div className="flex flex-col justify-center ">
                     <div className="w-full mt-[50px]">
                         <div className="">
-                            {/* <button className="btn bg-[#D2FE71] font-poppins text-xl font-normal text-[#000000]">
-                                <span className="material-symbols-outlined text-[#000000]">
-                                    arrow_back
-                                </span>
-                                Back
-                            </button> */}
                             <Backfunction path="/home"/>
                         </div>
                         <h1 className="text-6xl font-semibold text-[#8BCA00]  text-center">
@@ -24,15 +22,33 @@ const History = () => {
                         <hr className="border-y-[#8BCA00] w-full mt-[75px]" ></hr>
                     </div>
 
-                    <div className="flex ">
-                        <div className="block w-1/2 ">
+
+                    <div className="flex">
+                        <form className="card-body">
+                            <div className="flex gap-2 mt-6 justify-center">
+                                <button className="btn bg-[#8BCA00] w-1/3 h-16 md:h-10 font-poppins text-lg md:text-xl font-normal text-[#FFFFFF]">
+                                    Plan
+                                </button>
+
+                                <button className="btn bg-[#8BCA00] w-1/3 h-16 md:h-10 font-poppins text-lg md:text-xl font-normal text-[#FFFFFF]">
+                                    In progress
+                                </button>
+
+                                <button className="btn bg-[#8BCA00] w-1/3 h-16 md:h-10 font-poppins text-lg md:text-xl font-normal text-[#FFFFFF]">
+                                    Finish
+                                </button>
+
+
+
+                    {/* <div className="flex">
+                        <div className="block w-1/2">
                             <form className="card-body">
                                 <div className="form-control mt-6">
                                     <button className="btn bg-[#BDBDBD] font-poppins text-xl font-normal text-[#FFFFFF]">
                                         Plan
-                                    </button>
+                                    </button> */}
 
-                                    <div className="card card-side bg-base-100 shadow-xl w-full mt-9 border-2 border-[#BDBDBD]">
+                                    {/* <div className="card card-side bg-base-100 shadow-xl w-full mt-9 border-2 border-[#BDBDBD]">
                                         <div className="card-body p-1">
 
                                             <div className="hero-content flex-col lg:flex-row justify-between">
@@ -58,8 +74,9 @@ const History = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
+                                    </div> */}
+                                    
+                                {/* </div>
                             </form>
                         </div>
                         <div className="block w-1/2 ">
@@ -67,9 +84,9 @@ const History = () => {
                                 <div className="form-control mt-6">
                                     <button className="btn bg-[#8BCA00] font-poppins text-xl font-normal text-[#FFFFFF]">
                                         In progress
-                                    </button>
+                                    </button> */}
 
-                                    <div className="card card-side bg-base-100 shadow-xl w-full mt-9 border-2 border-[#8BCA00]">
+                                    {/* <div className="card card-side bg-base-100 shadow-xl w-full mt-9 border-2 border-[#8BCA00]">
                                         <div className="card-body p-1">
 
                                             <div className="hero-content flex-col lg:flex-row justify-between">
@@ -95,8 +112,8 @@ const History = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
+                                    </div> */}
+                                {/* </div>
                             </form>
                         </div>
                         <div className="block w-1/2 ">
@@ -104,9 +121,9 @@ const History = () => {
                                 <div className="form-control mt-6">
                                     <button className="btn bg-[#BDBDBD] font-poppins text-xl font-normal text-[#FFFFFF]">
                                         Finish
-                                    </button>
+                                    </button> */}
 
-                                    <div className="card card-side bg-base-100 shadow-xl w-full mt-9 border-2 border-[#BDBDBD]">
+                                    {/* <div className="card card-side bg-base-100 shadow-xl w-full mt-9 border-2 border-[#BDBDBD]">
                                         <div className="card-body p-1">
 
                                             <div className="hero-content flex-col lg:flex-row justify-between">
@@ -132,18 +149,12 @@ const History = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
+                                    </div> */}
                                 </div>
-                            </form>
-                        </div>
-
-
-
+                        </form>
                     </div>
-
                 </div>
             </div >
-
         </Layout>
         </>
     )
